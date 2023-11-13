f82c92f32fdb8fd364d978f50622d392aa326de4
ozdv <cam@ozdv.me>
2023-11-13 10:42:35 -0700

    backend eslint/prettier config

--- server/index.ts
+++ server/index.ts
@@ -1,19 +1,37 @@
-const express = require("express");
-const cors = require("cors");
+import express = require("express");
+import cors = require("cors");
+// Could chck out:
+// https://blog.logrocket.com/extend-express-request-object-typescript/
+
+//   Currently trying out this one:
+// https://plainenglish.io/blog/typed-express-request-and-response-with-typescript
+
+export interface TypedRequestBody<T> extends express.Request {
+  body: T;
+}
+
 const port = 3200;
-const app = express();
+const app: express.Application = express();
+
 app.use(cors());
 
-app.get("api/test", (req: any, res: any) => {
+app.get("api/test", (req: express.Request, res: express.Response) => {
   res.send({ data: "Hello from server" });
   console.log("apitest");
-});
-
-app.get("/api/home", (req: any, res: any) => {
-  res.json({ message: "Hello world!" });
-  console.log("apihome");
 });
 
 app.listen(port, () => {
   console.log(`Sever running on ${port}`);
 });
+
+app.post(
+  "/login",
+  (
+    req: TypedRequestBody<{ username: string; password: string }>,
+    res: express.Response
+  ) => {
+    const success = req.body.username === "foo" && req.body.password === "bar";
+
+    res.status(200).json({ Success: success });
+  }
+);
