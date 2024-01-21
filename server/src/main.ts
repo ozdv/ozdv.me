de0dffad7c6abad4763a2b72367af7ce74962e93
ozdv <cam@ozdv.me>
2024-01-21 06:41:40 -0700

    disable abort on error

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+// The entry file of the application
+// which uses the core function NestFactory to create a Nest application instance.
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { abortOnError: false });
   await app.listen(4000);
 }
 bootstrap();
