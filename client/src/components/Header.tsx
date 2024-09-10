f573606387630ca14f51ba31821d3e80b0c62cbc
ozdv <cam@ozdv.me>
2024-09-10 09:38:30 -0600

    Fix transition bug

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -3,15 +3,13 @@
 import { navItems } from "@/constants/navItems";
 import {
   Popover,
+  PopoverBackdrop,
   PopoverButton,
-  PopoverOverlay,
   PopoverPanel,
-  Transition,
-  TransitionChild,
 } from "@headlessui/react";
 import clsx from "clsx";
 import Link from "next/link";
-import React, { Fragment, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Container } from "./Container";
 import { ThemeToggle } from "./ThemeToggle";
 
@@ -86,39 +84,22 @@ const MobileNavigation = () => {
       >
         {({ open }) => <MobileNavIcon open={open} />}
       </PopoverButton>
-      <Transition>
-        <TransitionChild
-          as={Fragment}
-          enter="duration-150 ease-out"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="duration-150 ease-in"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-        >
-          <PopoverOverlay className="fixed inset-0 h-screen bg-white/75 dark:bg-zinc-900/75" />
-        </TransitionChild>
-        <TransitionChild
-          as={Fragment}
-          enter="duration-150 ease-out"
-          enterFrom="opacity-0 scale-95"
-          enterTo="opacity-100 scale-100"
-          leave="duration-100 ease-in"
-          leaveFrom="opacity-100 scale-100"
-          leaveTo="opacity-0 scale-95"
-        >
-          <PopoverPanel
-            as="div"
-            className="absolute inset-x-4 mt-4 flex origin-top flex-col rounded-2xl border bg-white p-4 shadow-xl ring-0 backdrop-blur backdrop-filter dark:bg-zinc-900"
-          >
-            {navItems.map((navItem) => (
-              <MobileNavLink href={navItem.href} key={navItem.href}>
-                {navItem.label}
-              </MobileNavLink>
-            ))}
-          </PopoverPanel>
-        </TransitionChild>
-      </Transition>
+
+      <PopoverBackdrop
+        transition
+        className="fixed inset-0 h-screen bg-white/75 dark:bg-zinc-900/75"
+      />
+
+      <PopoverPanel
+        transition
+        className="absolute inset-x-4 mt-4 flex origin-top flex-col rounded-2xl border bg-white p-4 shadow-xl ring-0 backdrop-blur backdrop-filter dark:bg-zinc-900"
+      >
+        {navItems.map((navItem) => (
+          <MobileNavLink href={navItem.href} key={navItem.href}>
+            {navItem.label}
+          </MobileNavLink>
+        ))}
+      </PopoverPanel>
     </Popover>
   );
 };
