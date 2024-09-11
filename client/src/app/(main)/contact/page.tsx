8074960aee3f9bae23dcf3bc7ed12a5480325a0f
ozdv <cam@ozdv.me>
2024-09-10 20:17:37 -0600

    padding

--- client/src/app/(main)/contact/page.tsx
+++ client/src/app/(main)/contact/page.tsx
@@ -4,7 +4,7 @@ import { Container } from "@/components/Container";
 const ContacPage = () => {
   return (
     <Container className="my-10">
-      <section className="space-y-8">
+      <section className="space-y-8 px-4">
         <div className="mx-auto max-w-2xl text-center">
           <h1 className="mb-6 text-center text-4xl font-bold leading-9 text-zinc-900 dark:text-zinc-100 sm:text-4xl">
             Contact me
