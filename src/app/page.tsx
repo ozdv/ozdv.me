533e6b8544cf767019ecac912efbeeee99874299
ozdv <cam@ozdv.me>
2025-10-29 13:31:40 -0600

    add theme-toggle component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { ThemeToggle } from "@/components/theme-toggle";
 import Image from "next/image";
 
 export default function Home() {
@@ -59,6 +60,7 @@ export default function Home() {
             Documentation
           </a>
         </div>
+        <ThemeToggle />
       </main>
     </div>
   );
