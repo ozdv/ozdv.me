c298747e1b7f8563d22ff4f08e568d6e30bbe9c2
ozdv <cam@ozdv.me>
2025-11-01 12:04:50 -0600

    refined some more app header kinks

--- src/components/app-header.tsx
+++ src/components/app-header.tsx
@@ -1,12 +1,15 @@
 "use client";
 
+import { MobileNav } from "@/components/mobile-nav";
 import { ThemeToggle } from "@/components/theme-toggle";
+import { Button } from "@/components/ui/button";
 import { NAV_ITEMS } from "@/lib/constants";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
-import { MobileNav } from "./mobile-nav";
+
+// todo fix backdrop blur animation on scroll
 
 export const AppHeader = () => {
   const [scroll, setScroll] = useState(false);
@@ -33,39 +36,43 @@ export const AppHeader = () => {
   return (
     <header
       className={cn(
-        "fixed inset-x-0 top-0 z-50 border-b backdrop-blur transition-colors duration-500 border-b-transparent",
+        "fixed inset-x-0 top-0 z-50 mx-auto border-b backdrop-blur transition-colors duration-500 border-b-transparent h-(--header-height) flex",
         scroll && "border-b-border",
       )}
     >
-      <nav className="mx-auto px-6 py-4 h-var(--header-height)  flex items-center justify-between">
-        <MobileNav className="flex md:hidden" />
-        <div className="flex items-center gap-8">
-          <Link
-            href="/"
-            aria-label="Home"
-            scroll={false}
-            className="hidden md:flex text-2xl text-muted-foreground transition-colors hover:text-foreground px-2 focus-visible:rounded focus-visible:outline-2 focus-visible:outline-offset-2 "
-          >
-            OZDV
-          </Link>
+      <div className="flex flex-row items-center justify-between w-full">
+        <nav className="flex flex-row items-center gap-4">
+          <MobileNav className="flex md:hidden" />
 
-          <div className="hidden md:flex items-center gap-6">
+          <Button
+            asChild
+            variant="ghost"
+            className="hidden lg:flex text-2xl font-normal"
+          >
+            <Link href="/">
+              OZDV
+              <span className="sr-only">Home</span>
+            </Link>
+          </Button>
+          <div className="hidden md:flex items-center gap-2">
             {NAV_ITEMS.map((navItem: { label: string; href: string }) => (
-              <Link
-                key={navItem.href}
-                href={navItem.href}
-                className={cn(
-                  "text-sm px-1 text-muted-foreground hover:text-foreground transition-colors font-semibold ",
-                  pathname === navItem.href && "text-foreground",
-                )}
-              >
-                {navItem.label}
-              </Link>
+              <Button key={navItem.href} variant="ghost" asChild size="sm">
+                <Link
+                  key={navItem.href}
+                  href={navItem.href}
+                  className={cn(
+                    "text-sm px-1 text-muted-foreground hover:text-foreground transition-colors font-semibold ",
+                    pathname === navItem.href && "text-foreground",
+                  )}
+                >
+                  {navItem.label}
+                </Link>
+              </Button>
             ))}
           </div>
-        </div>
+        </nav>
         <ThemeToggle />
-      </nav>
+      </div>
     </header>
   );
 };
--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -1,33 +1,44 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { useTheme } from "next-themes";
+import { useCallback } from "react";
 
+const ThemeIcon = (props: React.ComponentPropsWithoutRef<"svg">) => {
+  return (
+    <svg viewBox="0 0 24 24" aria-hidden="true" {...props}>
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M12 20a8 8 0 1 0 0-16 8 8 0 0 0 0 16Zm-5-8a5 5 0 0 0 5 5V7a5 5 0 0 0-5 5Z"
+      />
+    </svg>
+  );
+};
+
+// todo make this a fun animation and/or fix rotation animation
 export const ThemeToggle: React.FC = () => {
-  const { theme, setTheme } = useTheme();
+  const { resolvedTheme, setTheme } = useTheme();
 
-  const otherTheme = theme === "dark" ? "light" : "dark";
+  const toggleTheme = useCallback(() => {
+    setTheme(resolvedTheme === "dark" ? "light" : "dark");
+  }, [resolvedTheme, setTheme]);
 
   return (
-    <button
-      id="theme-toggle-button"
-      type="button"
-      className={cn(
-        "h-6 w-6 fill-muted-foreground hover:fill-foreground cursor-pointer",
-        "focus-visible:rounded-full focus-visible:outline-0 focus-visible:ring-2 focus-visible:ring-ring/50",
-        "transition-all duration-500",
-        { "-rotate-180": theme === "dark" },
-      )}
-      onClick={() => setTheme(otherTheme)}
+    <Button
+      variant="ghost"
+      size="icon"
+      title="Toggle theme"
+      className="fill-muted-foreground hover:fill-foreground"
+      onClick={toggleTheme}
     >
-      <span className="sr-only">Switch to {otherTheme} theme</span>
-      <svg viewBox="0 0 24 24" aria-hidden="true">
-        <path
-          fillRule="evenodd"
-          clipRule="evenodd"
-          d="M12 20a8 8 0 1 0 0-16 8 8 0 0 0 0 16Zm-5-8a5 5 0 0 0 5 5V7a5 5 0 0 0-5 5Z"
-        />
-      </svg>
-    </button>
+      <span className="sr-only">Switch theme</span>
+      <ThemeIcon
+        className={cn("size-6 transition-transform duration-500", {
+          "rotate-180": resolvedTheme === "dark",
+        })}
+      />
+    </Button>
   );
 };
