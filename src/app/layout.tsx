d3810c9340eed8d2a97850ae2568c80e5b4dd981
ozdv <cam@ozdv.me>
2025-11-02 09:50:42 -0700

    Add vercel speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { AppHeader } from "@/components/navigation/app-header";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -46,6 +47,7 @@ export default function RootLayout({
             className="flex-1 overflow-y-auto mt-(--header-height)"
           >
             {children}
+            <SpeedInsights />
           </main>
           <Toaster position="top-center" />
         </ThemeProvider>
